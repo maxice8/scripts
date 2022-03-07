d042e29d9723b32610547a796246badf04fc6326
maxice8 <thinkabit.ukim@gmail.com>
2022-03-07 08:44:04 -0300

    fgc: rewrite in shellscript

--- scripts/fgc.nix
+++ scripts/fgc.nix
@@ -1,10 +1,9 @@
 { pkgs }:
-pkgs.writeScriptBin "fgc"
+pkgs.writeShellScriptBin "fgc"
 ''
-#!${pkgs.rc}/bin/rc
-NOAPORTS_LINT=1
-NOAPKBUILD_LINT=1
-NOSECFIXES_LINT=1
-NOPKGVERCHECK=1
+NOAPORTS_LINT=1 \
+NOAPKBUILD_LINT=1 \
+NOSECFIXES_LINT=1 \
+NOPKGVERCHECK=1 \
 exec ${pkgs.git}/bin/git commit --no-edit $*
 ''
