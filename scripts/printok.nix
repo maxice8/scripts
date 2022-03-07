5787ab12b4d9b8045f3a84009cc49ce8d955a82e
maxice8 <thinkabit.ukim@gmail.com>
2022-03-07 09:05:19 -0300

    printok: rewrite in sh

--- scripts/printok.nix
+++ scripts/printok.nix
@@ -1,8 +1,7 @@
 { pkgs }:
-pkgs.writeScriptBin "printok"
+pkgs.writeShellScriptBin "printok"
 ''
-#!${pkgs.rc}/bin/rc
-if(test $#PRINTOK_QUIET -ne 0) { exit 0 }
-if(~ $#* 0) { exit 1 }
-${pkgs.coreutils}/bin/printf '\033[0m[ \033[32mOK\033[0m ] %s\n' $"*
+[ -n "''${PRINTOK_QUIET+set}" ] && exit 0
+[ $# -lt 1 ] && exit 1
+${pkgs.coreutils}/bin/printf '\033[0m[ \033[32mOK\033[0m ] %s\n' "$*"
 ''
