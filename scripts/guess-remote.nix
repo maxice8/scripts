74184c51610b2f5a8c5d1bd73fa8d8baa1070a54
maxice8 <thinkabit.ukim@gmail.com>
2022-03-08 14:11:04 -0300

    guess-remote: properly rewrite in sh

--- scripts/guess-remote.nix
+++ scripts/guess-remote.nix
@@ -1,14 +1,14 @@
 { pkgs }:
 pkgs.writeShellScriptBin "guess-remote"
   ''
-    if(! ${pkgs.git}/bin/git rev-parse --is-inside-work-tree >/dev/null 2>&1) exit 1
-    if(${pkgs.git}/bin/git config remote.upstream.url >/dev/null) {
+    ${pkgs.git}/bin/git rev-parse --is-inside-work-tree >/dev/null 2>&1 || exit 1
+    if ${pkgs.git}/bin/git config remote.upstream.url >/dev/null; then
       echo upstream
       exit 0
-    }
-    if(${pkgs.git}/bin/git config remote.origin.url >/dev/null) {
+    fi
+    if ${pkgs.git}/bin/git config remote.origin.url >/dev/null; then
       echo origin
       exit 0
-    }
+    fi
     exit 1
   ''
