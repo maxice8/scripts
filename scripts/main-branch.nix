815bd16d80c71f815a48acce8e9452583f8f11ab
maxice8 <thinkabit.ukim@gmail.com>
2022-03-11 20:35:19 -0300

    main-branch: rewrite in sh

--- scripts/main-branch.nix
+++ scripts/main-branch.nix
@@ -1,11 +1,10 @@
 { pkgs }:
-pkgs.writeScriptBin "main-branch"
+pkgs.writeShellScriptBin "main-branch"
   ''
-    #!${pkgs.rc}/bin/rc
-    if(! ${pkgs.git}/bin/git rev-parse --is-inside-work-tree >[2=] >[1=]) exit 1
-    if(~ $#* 0) *=(origin)
+    ${pkgs.git}/bin/git rev-parse --is-inside-work-tree >/dev/null 2>&1 || exit 1
+    [ $# -lt 1 ] && set -- origin
 
-    ref=`{${pkgs.git}/bin/git symbolic-ref refs/remotes/$1/HEAD >[2=] || echo master}
-    ref=`{${pkgs.coreutils}/bin/basename $ref >[2=]}
-    echo $ref
+    ref="$(${pkgs.git}/bin/git symbolic-ref refs/remotes/"$1"/HEAD 2>/dev/null || echo master)"
+    ref="$(${pkgs.coreutils}/bin/basename "$ref" 2>/dev/null)"
+    ${pkgs.coreutils}/bin/printf '%s' "$ref"
   ''
